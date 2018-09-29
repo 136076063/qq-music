6e0ee928cbe5d96aed56614be969be1af56509ee
136076063 <136076063@qq.com>
2018-09-29 21:20:43 +0800

    vue-router默认首页focu优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Recommend from 'components/pages/recommend.vue';
-import singer from 'components/pages/singer.vue';
-import search from 'components/pages/search.vue';
-import rank from 'components/pages/rank.vue';
+import Singer from 'components/pages/singer.vue';
+import Search from 'components/pages/search.vue';
+import Rank from 'components/pages/rank.vue';
 
 Vue.use(Router);
 
@@ -11,23 +11,24 @@ export default new Router({
     routes: [
         {
             path: '/',
-            redirect: '/Recommend' // 重定向路径
+            redirect: '/Recommend', // 重定向路径
+            component: Recommend
         },
         {
             path: '/Recommend', // 推荐
             component: Recommend
         },
         {
-            path: '/singer', // 歌手
-            component: singer
+            path: '/Singer', // 歌手
+            component: Singer
         },
         {
-            path: '/search', // 搜索
-            component: search
+            path: '/Search', // 搜索
+            component: Search
         },
         {
-            path: '/rank', // 排行
-            component: rank
+            path: '/Rank', // 排行
+            component: Rank
         }
     ]
 });
